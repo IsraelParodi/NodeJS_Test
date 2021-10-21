adc5f30bbed0920f505eab45211ccaac38b6b4d8
IsraelParodi <israelps97@gmail.com>
2021-10-21 14:45:37 -0500

    initial commit

---
+++ 3-Modules.js
@@ -0,0 +1,9 @@
+// Modules
+const names = require("./4-Names");
+const sayHi = require("./5-Utils");
+const data = require("./6-Alternative-flavor");
+require("./7-Mind-grenade");
+
+sayHi(names.john);
+sayHi(names.peter);
+console.log(data);
